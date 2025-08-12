65d103c1ed91fb363e860d643d7308c5db6a02a5
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-08-12 14:53:51 -0300

    criando estados

--- useCallback/src/App.jsx
+++ useCallback/src/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react'
 import './App.css'
+import List from './components/list'
 
 function App() {
 
+  const [text, setText] = useState('')
+  const [resourceType, setResourceType] = useState('todos')
+
+  const request = async ()  => {
+    const response = fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    const json = await response.json()
+    return json
+  }
+
   return (
     <>
-    
+      <input type="text" onChange={(event) => setText(event.target.value)} />
+      <button onClick={() => setResourceType('posts')}>posts</button>
+      <button onClick={() => setResourceType('comments')}>comments</button>
+      <button onClick={() => setResourceType('todos')}>todos</button>
+      <List items={request} />
     </>
   )
 
---
+++ useCallback/src/components/list.jsx
@@ -0,0 +1,9 @@
+const List = ({ items }) => {
+    return (
+        <ul>
+            
+        </ul>
+    )
+}   
+
+export default List
